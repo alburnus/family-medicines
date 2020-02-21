e298c70ae7e561496f6b456205a583d62e94409c
Krzysztof <k.ukleja@gmail.com>
2020-02-21 23:34:32 +0100

    Init project and POC for get medicines from website

---
+++ src/main/java/com/github/alburnus/FamilyMedicinesApplication.java
@@ -0,0 +1,13 @@
+package com.github.alburnus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMedicinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMedicinesApplication.class, args);
+	}
+
+}
